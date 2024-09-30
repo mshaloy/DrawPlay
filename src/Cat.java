f349657c64ff9ab103bca7b5ac94e7495305e7b2
wsmolko17 <wsmolko@mines.edu>
2024-09-29 19:03:34 -0600

    Liam Smolko - cloned repository, changed head size and eye color

--- src/Cat.java
+++ src/Cat.java
@@ -17,8 +17,8 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 4;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -30,7 +30,7 @@ public class Cat {
 		g2.setColor(Color.black);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.blue);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
