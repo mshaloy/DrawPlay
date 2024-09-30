e2931b3a3748dccfa8b066dd2facaa5067080eca
Mallory <mallory_shaloy@mines.edu>
2024-09-29 19:16:43 -0600

    Changed head color to gray. Changed mouth dimensions to be smaller.

--- src/Cat.java
+++ src/Cat.java
@@ -17,8 +17,8 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 3;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 4;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -27,7 +27,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		g2.setColor(Color.gray);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.blue);
